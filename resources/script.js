da942c6bf33ac41b6abe59c0ef1dec02e2dd163f
hannah914 <hannah.pratsch14@gmail.com>
2024-10-28 18:51:17 -0500

    comments

--- resources/script.js
+++ resources/script.js
@@ -1,15 +1,18 @@
+// Toggle menu icon and navbar visibility
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
-let navItems = document.querySelectorAll('.navbar a'); // Adjust the selector based on your HTML
+
+// Close navbar when a link is clicked
+let navItems = document.querySelectorAll('.navbar a');
 
 menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navbar.classList.toggle('open');
+    menu.classList.toggle('bx-x'); // Change icon
+    navbar.classList.toggle('open'); // Show/hide navbar
 };
 
 navItems.forEach(item => {
     item.onclick = () => {
-        menu.classList.remove('bx-x');
-        navbar.classList.remove('open');
+        menu.classList.remove('bx-x'); // Reset icon
+        navbar.classList.remove('open'); // Close navbar
     };
 });
